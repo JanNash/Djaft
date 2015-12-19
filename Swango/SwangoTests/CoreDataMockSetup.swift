199fa3e57037db94c8325fd8b01297b6ca02a3ae
Jan Nash <jnash@jnash.de>
2015-12-19 20:27:23 +0100

    Created a BaseTest, created a mock datamodel.

--- Swango/SwangoTests/CoreDataMockSetup.swift
+++ Swango/SwangoTests/CoreDataMockSetup.swift
@@ -7,8 +7,42 @@
 //
 
 import Foundation
+import CoreData
 
 
+// Public Interface
+extension CoreDataMockSetup {
+    // Variable Class Properties
+    class func mainObjectContext() -> NSManagedObjectContext {return self._mainObjectContext}
+}
+
+
+// Main Implementation
 class CoreDataMockSetup: AnyObject {
+    private static var __mainObjectContext: NSManagedObjectContext!
+    private static var _mainObjectContext: NSManagedObjectContext {
+        if self.__mainObjectContext == nil {
+            self._createMainMOC()
+        }
+     return self.__mainObjectContext
+    }
     
+    private class func _createMainMOC() {
+        let bundle: NSBundle = NSBundle(forClass: self)
+        let modelURL = bundle.URLForResource("SwangoUnitTestDataModel", withExtension: "momd")
+        let mom = NSManagedObjectModel(contentsOfURL: modelURL!)
+        let psc = NSPersistentStoreCoordinator(managedObjectModel: mom!)
+        let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
+        let storeURL = urls.last?.URLByAppendingPathComponent("SwangoUnitTestDB.sqlite")
+        
+        do {
+            try psc.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: storeURL, options: nil)
+        } catch let error as NSError {
+            fatalError("Failed setting up Database with error \(error)")
+        }
+        
+        self.__mainObjectContext = NSManagedObjectContext(concurrencyType: .MainQueueConcurrencyType)
+        self.__mainObjectContext!.persistentStoreCoordinator = psc
+        print("Successfully set up Swango unit-test database")
+    }
 }
---
+++ Swango/SwangoTests/SwangoBaseTest.swift
@@ -0,0 +1,40 @@
+//
+//  SwangoTest.swift
+//  SwangoTests
+//
+//  Created by Jan Nash on 12/19/15.
+//  Copyright © 2015 Jan Nash. All rights reserved.
+//
+
+import XCTest
+import CoreData
+@testable import Swango
+
+class SwangoTest: XCTestCase {
+    
+    var mainObjectContext: NSManagedObjectContext = CoreDataMockSetup.mainObjectContext()
+    
+//    override func setUp() {
+//        super.setUp()
+//        // Put setup code here. This method is called before the invocation of each test method in the class.
+//    }
+//
+//    override func tearDown() {
+//        // Put teardown code here. This method is called after the invocation of each test method in the class.
+//        super.tearDown()
+//    }
+//
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(self.mainObjectContext == CoreDataMockSetup.mainObjectContext())
+    }
+//
+//    func testPerformanceExample() {
+//        // This is an example of a performance test case.
+//        self.measureBlock {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
+    
+}
