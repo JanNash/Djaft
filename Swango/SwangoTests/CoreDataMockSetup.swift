0b0c2cb8cade905b989d5acf23524ac80bde0d59
Jan Nash <jnash@jnash.de>
2015-12-19 19:48:02 +0100

    Let's write some tests!

---
+++ Swango/SwangoTests/CoreDataMockSetup.swift
@@ -0,0 +1,14 @@
+//
+//  CoreDataMockSetup.swift
+//  Swango
+//
+//  Created by Jan Nash on 12/19/15.
+//  Copyright © 2015 Jan Nash. All rights reserved.
+//
+
+import Foundation
+
+
+class CoreDataMockSetup: AnyObject {
+    
+}
